f689d99a7c54cdfe71e6a043ef63ba704551bcf2
Haroun Ach <harounacho21@gmail.com>
2020-12-22 16:21:16 +0100

    make navigation drawer for mobile

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,21 @@
-console.log("App loaded");
+$(document).ready(function () {
+  /* MVC model */
+  const model = {
+    /* Initialize Model */
+    init: function () {},
+  };
+
+  /* MVC controller */
+  const controller = {
+    /* Initialize Controller */
+    init: function () {},
+  };
+
+  /* MVC View */
+  const view = {
+    init: function () {
+      // Get references
+      this.menuBtn = $(".menu-btn");
+    },
+  };
+});
