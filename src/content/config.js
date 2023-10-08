27bfd145794f2e9704b7244d1830e371f092a336
Haroun Ach <harounacho21@gmail.com>
2023-10-08 21:08:13 +0100

    Create projects section

--- src/content/config.js
+++ src/content/config.js
@@ -11,8 +11,27 @@ const certsCollection = defineCollection({
     }),
   ),
 });
+
+const projectCollection = defineCollection({
+  type: "data",
+  schema: z.array(
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      url: z.string(),
+      img: z.object({
+        src: z.string(),
+        alt: z.string(),
+      }),
+      tag: z.string(),
+      featured: z.boolean().optional(),
+    }),
+  ),
+});
+
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
   certifications: certsCollection,
+  projects: projectCollection,
 };
